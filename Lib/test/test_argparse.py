de4115366dbfcbe50fb4c394d3f29cc831d1b621
Cyker Way <cykerway@gmail.com>
2020-01-29 08:14:11 +0800

    Format positional arguments using spaces.
    Optional arguments use spaces to separate arguments, so we do the same
    thing with positional arguments.

--- Lib/test/test_argparse.py
+++ Lib/test/test_argparse.py
@@ -4143,7 +4143,7 @@ class TestHelpTupleMetavarPositional(HelpTestCase):
     help = usage + '''\
 
         positional arguments:
-          bar, baz    foo help
+          bar baz     foo help
 
         optional arguments:
           -h, --help  show this help message and exit
